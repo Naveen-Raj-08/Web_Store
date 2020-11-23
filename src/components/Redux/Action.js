e80d1fa16a8fba9b675f9ec8fd7b0abc2c3a6b13
Naveen <rajnaveen677@gmail.com>
2020-11-23 21:25:01 +0530

    commit5

---
+++ src/components/Redux/Action.js
@@ -0,0 +1,6 @@
+export const ADD_ITEMS = "ADD_ITEMS";
+export const REMOVE_ITEMS = "REMOVE_ITEMS";
+
+//Actions
+export const ADD = () => ({ type: ADD_ITEMS });
+export const REMOVE = () => ({ type: REMOVE_ITEMS });
